ca94a22501ba5f2bddc7ee88b68dde6f2b0d6d8a
Pablo Ramos <pablo-362@hotmail.com>
2021-11-26 21:49:50 -0300

    pomodoro logic end

--- main.js
+++ main.js
@@ -1,53 +1,117 @@
-const minutes = document.querySelector('.minutes');
-const seconds = document.querySelector('.seconds');
+// VARIABLES DECLARATIONS
+const minutesFocus = document.querySelector('.minutes');
+const secondsFocus = document.querySelector('.seconds');
 const minutesBreak = document.querySelector('.minutes-break');
 const secondsBreak = document.querySelector('.seconds-break');
-const descanso = document.querySelector('.descanso');
-const playTimer = document.querySelector('.playTimer');
-const minutesDefault = 25;
-let currentMinutes; let currentSeconds; let currentMinutesBreak; let currentSecondsBreak; let intervalMinutes; let intervalSeconds; let
-  minutesFocus;
-// STARTING CONDITIONS
+const playFocus = document.querySelector('.playTimer');
+const reset = document.querySelector('.reset');
+const timeoutSound = document.getElementById('timeout');
+let currentMinutesFocus; let currentSecondsFocus; let currentMinutesBreak; let
+  currentSecondsBreak;
+let intervalMinutesFocus; let intervalSecondsFocus; let intervalMinutesBreak; let
+  intervalSecondsBreak;
+
+// STARTING CONDITIONS START
 function startingConditionsFocus() {
-  currentMinutes = 3;
-  currentSeconds = 3;
-  minutes.innerHTML = currentMinutes;
-  seconds.innerHTML = currentSeconds;
+  currentMinutesFocus = 24;
+  currentSecondsFocus = 59; // This value have to be 59'' at the beginning;
+  minutesFocus.innerHTML = currentMinutesFocus;
+  secondsFocus.innerHTML = currentSecondsFocus;
 }
 function startingConditionsBreak() {
-  currentMinutesBreak = 5;
-  currentSecondsBreak = 5;
+  currentMinutesBreak = 4;
+  currentSecondsBreak = 59;
   minutesBreak.innerHTML = currentMinutesBreak;
   secondsBreak.innerHTML = currentSecondsBreak;
 }
 startingConditionsFocus();
 startingConditionsBreak();
+// STARTING CONDITIONS END
 
-// START TIMER
-// setInterval execute a function every certain interval
-playTimer.addEventListener('click', () => {
-  intervalMinutes = setInterval(minutesTimer, 3000); // Reemplazar este valor por minutesDefault*1000 cuando este terminada la app
-  intervalSeconds = setInterval(secondsTimer, 1000);
-});
-
-// This function execute it every 60'', substract 1 minute and print it
-function minutesTimer() {
-  currentMinutes -= 1;
-  minutes.innerHTML = currentMinutes;
-  console.log(currentMinutes);
-}
-// This function execute it every second, substract 1 sec and verify if the countwdown reach 0
-function secondsTimer() {
-  currentSeconds -= 1;
-  seconds.innerHTML = currentSeconds;
-  console.log(currentSeconds);
-
-  if (currentSeconds === 0) {
-    if (currentMinutes === 0) {
-      clearInterval(intervalSeconds);
-      clearInterval(intervalMinutes);
-      startingConditions();
+// BREAK SECTION
+function secondsTimerBreak() {
+  currentSecondsBreak -= 1;
+  // Adding 0 for numbers < 10
+  function addZeroBreak() {
+    if (currentSecondsBreak < 10) {
+      secondsBreak.innerHTML = `0${currentSecondsBreak}`;
+    } else {
+      secondsBreak.innerHTML = currentSecondsBreak;
     }
-    currentSeconds = 3;
+  }
+  addZeroBreak();
+
+  if (currentSecondsBreak === 0) {
+    if (currentMinutesBreak === 0) {
+      clearInterval(intervalSecondsBreak);
+      clearInterval(intervalMinutesBreak);
+      startingConditionsBreak();
+      document.querySelector('.focus').classList.add('active');
+      document.querySelector('.sleep').classList.remove('active');
+
+      timeoutSound.play();
+    }
+    currentSecondsBreak = 60; // Reset the second countdown
   }
 }
+function minutesTimerBreak() {
+  currentMinutesBreak -= 1;
+  minutesBreak.innerHTML = currentMinutesBreak;
+}
+
+// FOCUS SECTION
+// This function execute it every second, substract 1 sec and verify if the countwdown reach 0
+function secondsTimerFocus() {
+  currentSecondsFocus -= 1;
+  // Adding 0 for numbers < 10
+  function addZeroFocus() {
+    if (currentSecondsFocus < 10) {
+      secondsFocus.innerHTML = `0${currentSecondsFocus}`;
+    } else {
+      secondsFocus.innerHTML = currentSecondsFocus;
+    }
+  }
+  addZeroFocus();
+
+  if (currentSecondsFocus === 0) {
+    if (currentMinutesFocus === 0) {
+      clearInterval(intervalSecondsFocus);
+      clearInterval(intervalMinutesFocus);
+      startingConditionsFocus();
+      timeoutSound.play();
+      document.querySelector('.focus').classList.remove('active');
+      document.querySelector('.sleep').classList.add('active');
+
+      // Start break time
+      intervalMinutesBreak = setInterval(minutesTimerBreak, 60000); // Reemplazar luego por 60000
+      intervalSecondsBreak = setInterval(secondsTimerBreak, 1000);
+    }
+    currentSecondsFocus = 60; // Reset the second countdown
+  }
+}
+
+// This function execute it every 60'', substract 1 minute and print it
+function minutesTimerFocus() {
+  currentMinutesFocus -= 1;
+  minutesFocus.innerHTML = currentMinutesFocus;
+}
+
+// Start focus timer
+// setInterval execute a function every certain interval
+playFocus.addEventListener('click', () => {
+  document.querySelector('.focus').classList.add('active');
+  intervalMinutesFocus = setInterval(minutesTimerFocus, 60000); // Reemplazar luego por 60000
+  intervalSecondsFocus = setInterval(secondsTimerFocus, 1000);
+});
+
+// RESET BUTTON
+reset.addEventListener('click', () => {
+  startingConditionsFocus();
+  startingConditionsBreak();
+  clearInterval(intervalSecondsBreak);
+  clearInterval(intervalMinutesBreak);
+  clearInterval(intervalSecondsFocus);
+  clearInterval(intervalMinutesFocus);
+  document.querySelector('.sleep').classList.remove('active');
+  document.querySelector('.focus').classList.remove('active');
+});
